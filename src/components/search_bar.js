38ec4d938dc0254ae78876e69059c419d5727864
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-04-06 01:51:22 -0400

    Implemented new styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,6 +10,7 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="search-bar">
+        <div className="navbar-brand nav-title">ReactTube</div>
         <input
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)} />
